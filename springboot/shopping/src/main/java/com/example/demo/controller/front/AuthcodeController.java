9ea412b50ae2bdaed6e8e1feb12d7470469ad326
USER <USER@DESKTOP-M3A6088.mshome.net>
2025-07-04 13:25:55 +0800

    07041325

--- springboot/shopping/src/main/java/com/example/demo/controller/front/AuthcodeController.java
+++ springboot/shopping/src/main/java/com/example/demo/controller/front/AuthcodeController.java
@@ -11,7 +11,7 @@ import javax.imageio.ImageIO;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,6 +27,7 @@ import jakarta.servlet.http.HttpSession;
 @RequestMapping("/api")
 public class AuthcodeController {
 
+	@Autowired
 	private Producer captchaProducer;
 
 	
